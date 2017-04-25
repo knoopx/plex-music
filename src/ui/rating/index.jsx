9bdefc38f4a766888c040856e02d561fe7d268a7
Victor Martinez <knoopx@gmail.com>
2017-04-25 12:46:05 +0200

    FIX: Incorrect ratings

--- src/ui/rating/index.jsx
+++ src/ui/rating/index.jsx
@@ -1,3 +1,5 @@
+// @flow
+
 import React from 'react'
 import StarIcon from 'react-icons/lib/fa/star'
 import OpenStarIcon from 'react-icons/lib/fa/star-o'
@@ -14,17 +16,18 @@ export default class Rating extends React.PureComponent {
 
   static defaultProps = {
     value: 0,
-    max: 5,
+    max: 10,
+    stars: 5,
   }
 
   renderStar(index: number) {
-    const { size, value, style } = this.props
+    const { size, max, stars, value, style } = this.props
+    const IconName = (index / stars * max) <= value - 1 ? StarIcon : OpenStarIcon
 
-    const IconName = index <= value - 1 ? StarIcon : OpenStarIcon
     return (
       <IconName
         key={index}
-        onClick={(e) => { e.stopPropagation(); this.props.onChange(index + 1) }}
+        onClick={(e) => { e.stopPropagation(); this.props.onChange((index / stars * max) + 1) }}
         style={{ margin: 2, ...style }}
         size={size}
       />
@@ -32,8 +35,7 @@ export default class Rating extends React.PureComponent {
   }
 
   render() {
-    const { max } = this.props
-    const stars = Array.from(Array(max).keys())
-    return <div>{stars.map(index => this.renderStar(index))}</div>
+    const { stars } = this.props
+    return <div>{Array.from(Array(stars).keys()).map(index => this.renderStar(index))}</div>
   }
 }
