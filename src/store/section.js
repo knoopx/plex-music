f8eb6f3e23994fb59be7483addee42b89861c8e9
Victor Martinez <knoopx@gmail.com>
2018-03-07 20:49:34 +0100

    Updated deps

--- src/store/section.js
+++ src/store/section.js
@@ -2,7 +2,7 @@
 
 import _ from 'lodash'
 
-import { types, getParent, process as asyncAction } from 'mobx-state-tree'
+import { types, getParent, flow } from 'mobx-state-tree'
 import { parse as parseAlbum } from 'store/album'
 import { parse as parseArtist } from 'store/artist'
 
@@ -37,16 +37,23 @@ export default types
       albums.forEach(album => self.albumMap.put(album))
       console.timeEnd('setAlbums')
     },
-    fetchAlbums: asyncAction(function* (query = { excludeFields: ['summary', 'parentThumb', 'originallyAvailableAt'] }) {
+    fetchAlbums: flow(function* (query = {
+      excludeFields: ['summary', 'parentThumb', 'originallyAvailableAt'],
+    }) {
       console.time('fetchAlbums')
       const doc = yield self.device.request(`/library/sections/${self.id}/all`, { type: 9, ...query })
       if (!doc.MediaContainer || !_.isArray(doc.MediaContainer.Metadata)) { throw new Error('Unexpected response') }
       self.setAlbums(_.map(doc.MediaContainer.Metadata, item => parseAlbum(item)))
       console.timeEnd('fetchAlbums')
     }),
-    async findAllArtists(query = {}) {
-      const doc = await self.device.request(`/library/sections/${self.id}/all`, { type: 8, ...query })
-      if (!doc._children || !_.isArray(doc._children)) { throw new Error('Unexpected response') }
+    findAllArtists: flow(function* (query = {}) {
+      const doc = yield self.device.request(
+        `/library/sections/${self.id}/all`,
+        { type: 8, ...query },
+      )
+      if (!doc._children || !_.isArray(doc._children)) {
+        throw new Error('Unexpected response')
+      }
       return _.map(doc._children, item => parseArtist(item))
-    },
+    }),
   }))
